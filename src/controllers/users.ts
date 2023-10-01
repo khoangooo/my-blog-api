c065d4a685a996c86d549a9f1c6e5eae641f18f1
Khoa Ngo <ngokhoa9596@gmail.com>
2023-10-01 21:07:39 +0700

    update refresh token function

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -15,28 +15,33 @@ const login = async (req: Request, res: Response): Promise<void> => {
     const user: IUser | null = await User.findOne({ username });
 
     if (user?.password) {
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result) {
-          const data = user.toObject() as Partial<IUser>;
-          delete data.password
-          const accessTokenSecretKey = process.env.JWT_ACCESS_TOKEN_SECRET_KEY as Secret;
-          const refreshTokenSecretKey = process.env.JWT_REFRESH_TOKEN_SECRET_KEY as Secret;
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        const data = user.toObject() as Partial<IUser>;
+        delete data.password
+        const accessTokenSecretKey = process.env.JWT_ACCESS_TOKEN_SECRET_KEY as Secret;
+        const refreshTokenSecretKey = process.env.JWT_REFRESH_TOKEN_SECRET_KEY as Secret;
 
-          const accessToken = jwt.sign(data, accessTokenSecretKey, { expiresIn: ACCESS_TOKEN_EXPIRES_IN + "m" })
-          const refreshToken = jwt.sign(data, refreshTokenSecretKey, { expiresIn: REFRESH_TOKEN_EXPIRES_IN + "days" })
+        const accessToken = jwt.sign(data, accessTokenSecretKey, { expiresIn: ACCESS_TOKEN_EXPIRES_IN + "m" })
+        const refreshToken = jwt.sign(data, refreshTokenSecretKey, { expiresIn: REFRESH_TOKEN_EXPIRES_IN + "days" })
 
-          res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            sameSite: 'none',
-            secure: true,
-            maxAge: parseInt(REFRESH_TOKEN_EXPIRES_IN) * 24 * 60 * 60 * 1000 // 30 days
-          });
+        res.cookie('refreshToken', refreshToken, {
+          httpOnly: true,
+          sameSite: 'none',
+          secure: true,
+          maxAge: parseInt(REFRESH_TOKEN_EXPIRES_IN) * 24 * 60 * 60 * 1000 // 30 days
+        });
 
-          res.status(200).json({ status: true, data: { token: accessToken, timeout: Date.now() } });
-        } else {
-          res.status(401).json({ status: false, message: 'Passwords do not match' });
-        }
-      });
+        res.status(200).json({
+          status: true,
+          data: {
+            token: accessToken,
+            timeout: Date.now() + parseInt(ACCESS_TOKEN_EXPIRES_IN) * 60 * 1000
+          }
+        });
+      } else {
+        res.status(401).json({ status: false, message: 'Passwords do not match' });
+      }
     } else {
       res.status(404).json({ status: false, message: 'User is not existed' })
     }
@@ -87,8 +92,14 @@ const refresh = async (req: Request, res: Response): Promise<void> => {
       res.status(401).json({ status: false, message: 'Invalid refresh token' });
     }
 
-    const accessToken = jwt.sign(decoded, accessTokenSecretKey, { expiresIn: ACCESS_TOKEN_EXPIRES_IN + "m" });
-    res.status(200).json({ status: true, data: { token: accessToken, refreshToken, timeout: Date.now() } });
+    const accessToken = jwt.sign(decoded, accessTokenSecretKey);
+    res.status(200).json({
+      status: true,
+      data: {
+        token: accessToken,
+        timeout: Date.now() + parseInt(ACCESS_TOKEN_EXPIRES_IN) * 60 * 1000
+      }
+    });
 
   } catch (error) {
     res.status(500).send(getErrorMessage(error));
